b001855e254805a282bc43ccd083818b8f6eac3f
daw23-06 <daw23-06@c16x18.iescierva.net>
2024-02-05 19:51:02 +0100

    Anadir cambios

--- Principal.java
+++ Principal.java
@@ -1,5 +1,13 @@
+import java.time.LocalDateTime;
+
 public class Principal{
     public static void main(String[] args){
         System.out.println("Hola mundo");
+    }
+
+    public static void fechaYhora(){
+        LocalDateTime actual = LocalDateTime.now();
+
+        String a = "Fecha: " + actual.getDayOfMonth() + "/" + actual.getMonth() + "/" + actual.getYear() + "\nHora: " + actual.getHour() + ":" + actual.Minute() + ":" + actual.Second();
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Practica4.1
 Git inicial
+
+## Principal.java
+> Este fichero es un ejemplo que muestra un hola mundo y con un metodo que muestra la fecha y hora actual.
